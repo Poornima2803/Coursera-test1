3b53bb6348d58455d2251eda33f1667756983d3c
poornima <poornima@coursera.com>
2020-05-30 20:03:40 +0530

    First web file

---
+++ app.js
@@ -0,0 +1,38 @@
+(function () {
+'use strict';
+
+angular.module('myFirstApp', [])
+
+.controller('MyFirstController', function ($scope) {
+  $scope.itemname="";
+  $scope.str="";
+  $scope.calculateitem = function() {
+  var res=0;
+  res=checkLunch($scope.itemname);
+   if($scope.itemname=='')
+   {
+     return "Please enter data first";
+   }
+  else if(res>0&&res<3)
+  {
+    return "Enjoy!";
+  }
+  else if(res>3) {
+    return "Too much!";
+  }
+
+  };
+  function checkLunch(string){
+    $scope.result="words";
+
+    var words=[];
+    words=string.split(',');
+    var l=words.length;
+    console.log(l);
+    return l;
+  }
+// console.log(calculateitem());
+
+});
+
+})();
